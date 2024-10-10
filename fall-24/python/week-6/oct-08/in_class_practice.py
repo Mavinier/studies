d1fbbe0af227371123bee1f36a4c572113b15a1a
Mavinier <mavinier2010@gmail.com>
2024-10-10 14:22:24 -0300

    update folder structure

--- fall-24/python/week-6/oct-08/in_class_practice.py
+++ fall-24/python/week-6/oct-08/in_class_practice.py
@@ -1,5 +1,5 @@
-input_file_path = "/home/marlon/studies/fall-24/python/week-6/input.txt"
-output_file_path = "/home/marlon/studies/fall-24/python/week-6/output.txt"
+input_file_path = "/home/marlon/studies/fall-24/python/week-6/oct-8/input.txt"
+output_file_path = "/home/marlon/studies/fall-24/python/week-6/oct-8/output.txt"
 
 with open(input_file_path, 'r') as input_file:
   content = input_file.read()
@@ -12,9 +12,9 @@ with open(output_file_path, 'w') as output_file:
 print(f"\nContent successfully copied to {output_file_path}")
 
 
-input_file1_path = "/home/marlon/studies/fall-24/python/week-6/input1.txt"
-input_file2_path = "/home/marlon/studies/fall-24/python/week-6/input2.txt"
-output_merged_file = "/home/marlon/studies/fall-24/python/week-6/merged_output.txt" 
+input_file1_path = "/home/marlon/studies/fall-24/python/week-6/oct-8/input1.txt"
+input_file2_path = "/home/marlon/studies/fall-24/python/week-6/oct-8/input2.txt"
+output_merged_file = "/home/marlon/studies/fall-24/python/week-6/oct-8/merged_output.txt" 
 
 with open(input_file1_path, 'r') as input_file1:
   content1 = input_file1.read()
